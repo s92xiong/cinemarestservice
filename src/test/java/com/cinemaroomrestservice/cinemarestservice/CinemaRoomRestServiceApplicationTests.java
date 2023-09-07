60488529b10d1eca70611aa8d6c5de3902520305
Sheuh Xiong <sheuhxiong@gmail.com>
2023-09-07 13:23:02 -0400

    First commit

---
+++ src/test/java/com/cinemaroomrestservice/cinemarestservice/CinemaRoomRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaroomrestservice.cinemarestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRoomRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
